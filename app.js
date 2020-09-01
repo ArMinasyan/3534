23b218244f99b05882caa992c607b474e272ec18
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-09-01 11:44:54 +0400

    deleted DB files

--- app.js
+++ app.js
@@ -43,75 +43,10 @@ let GetObjectValues = (obj, temp = {}) => {
     }
 }
 
-app.get('/insert', function (req, res) {
-    let arr = [];
-    axios.get('https://lib-mtconnect-cartridge.azurewebsites.net/current').then(res => {
-
-        let data = JSON.parse(xml_js.xml2json(res.data, { compact: true })).
-            MTConnectStreams.Streams.DeviceStream;
-
-        let temp = {};
-
-        for (let st of data) {
-            GetObjectValues(st, temp);
-
-            arr.push(temp);
-            temp = {};
-        }
-
-        console.log(arr);
-    })
-})
-// console.log(data.MTConnectStreams.Streams.DeviceStream[0].ComponentStream[1].Events);
-//fs.writeFileSync('./test.json', data.MTConnectStreams.Streams[0].DeviceStream.ComponentStream[1].Events);
-// let TempDataArray = [];
-
-// for (let i = 0; i < data.length; i++) {
-//     let new_data = {
-//         manufacturer: data[i]._attributes.manufacturer,
-//         serialNumber: data[i]._attributes.serialNumber,
-//         timestamp: new Date(data[i]._attributes.timestamp),
-//         deviceUuid: data[i]._attributes.deviceUuid,
-//         assetId: data[i]._attributes.assetId,
-//         ManufacturingData_Description: data[i].ManufacturingData.Description._text,
-//         UUID: data[i].ManufacturingData.UUID._text,
-//         PartNumber: data[i].ManufacturingData.PartNumber._text,
-//         PartNumberRevision: data[i].ManufacturingData.PartNumberRevision._text,
-//         CartridgeType: data[i].ManufacturingData.CartridgeType._text,
-//         CartridgeDesignRevision: data[i].ManufacturingData.CartridgeDesignRevision._text,
-//         ManufacturingDate: data[i].ManufacturingData.ManufacturingDate._text,
-//         ManufacturingTestStatus: data[i].ManufacturingData.ManufacturingTestStatus._text,
-//         OperationalData_Description: data[i].OperationalData.Description._text,
-//         ArcTime: parseInt(data[i].OperationalData.ArcTime._text),
-//         PilotTime: parseInt(data[i].OperationalData.PilotTime._text),
-//         TransferTime: parseInt(data[i].OperationalData.TransferTime._text),
-//         NumberOfStarts: parseInt(data[i].OperationalData.NumberOfStarts._text),
-//         NumberOfTransfers: parseInt(data[i].OperationalData.NumberOfTransfers._text),
-//         Faults: data[i].OperationalData.Faults._attributes.faultCount > 0 ? FaulttoStringArray(data[i].OperationalData.Faults.Fault) : '',
-//         EndOfLifeEventCount: parseInt(data[i].OperationalData.EndOfLifeEventCount._text)
-//     }
-
-//     TempDataArray.push(new_data);
-// }
-
-// machine.bulkCreate(TempDataArray);
-
 
 
 
 app.get('/item', function (req, res) {
-    let filterBy = '', value = '';
-
-    if (!_.isEmpty(req.query)) {
-        for (const key in req.query) {
-            filterBy = key.toString();
-            value = req.query[key].toString()
-        }
-    }
-
-
-
-
     let avg_of_starts = 0, avg_of_transfers = 0, avg_of_arc_hours = 0;
     axios.get('https://lib-mtconnect-cartridge.azurewebsites.net/assets').then(result => {
 
@@ -191,17 +126,9 @@ app.get('/item', function (req, res) {
         // console.log(DataArray);
     })
 
-
-
-
 })
 
-//  http://localhost:8000/item
-//  http://localhost:8000/item?id=
-//  http://localhost:8000/item?manufacturer=
-//  http://localhost:8000/item?assetId=
-//  http://localhost:8000/item?UUID=
-//  http://localhost:8000/item?serialNumber=
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views', 'DataReplicated.html'));
