52aef91cbf34115cc7f057f9dee8447f30f3a231
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-09-07 16:21:25 +0400

    changes

--- routes/main.js
+++ routes/main.js
@@ -8,7 +8,8 @@ route.get('/:name', defender, function (req, res) {
 })
 
 route.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../views', 'Account.html'));
+    if (req.cookies.token) res.sendFile(path.join(__dirname, '../', 'views', 'DataReplicated.html')); else
+        res.sendFile(path.join(__dirname, '../views', 'Account.html'));
 })
 
 
