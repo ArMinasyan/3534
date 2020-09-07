f119afb6f8cee55acaf8d3ad623239230af6b2c3
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-09-07 18:15:33 +0400

    confirm change

--- routes/confirm.js
+++ routes/confirm.js
@@ -7,16 +7,20 @@ route.post('/confirm', function (req, res) {
     user.findOneAndUpdate({ $and: [{ email: req.body.email }, { token: req.body.token }] }, { token: '-' }, function (err, doc) {
 
         if (doc) {
-            let token = jwt.sign({ id: doc._id, time: Date.now() }, fs.readFileSync('./keys/Private.key'), { algorithm: "RS512" });
-            res.cookie("token", token, {
-                sameSite: true,
-                httpOnly: true,
-                maxAge: 10 * 36000
-            });
+            bcrypt.compare(req.body.password, doc.password, function (err, same) {
+                if (same) {
+                    let token = jwt.sign({ id: doc._id, time: Date.now() }, fs.readFileSync('./keys/Private.key'), { algorithm: "RS512" });
+                    res.cookie("token", token, {
+                        sameSite: true,
+                        httpOnly: true,
+                        maxAge: 10 * 36000
+                    });
 
-            res.json({ 'login': true })
-        }
-        else res.json({ 'msg': "Invalid token" });
+                    res.json({ 'login': true })
+                } else res.json({ 'msg': "Incorrect username and/or password" });
+
+            })
+        } else res.json({ 'msg': "Invalid token" });
     })
 })
 
