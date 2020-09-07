4054455e80d2b39b5b8c2ded943740ee17d69e6a
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-09-07 18:20:56 +0400

    change confirm

--- routes/confirm.js
+++ routes/confirm.js
@@ -3,24 +3,27 @@ let user = require('../models/user');
 let jwt = require('jsonwebtoken');
 let fs = require('fs');
 let path = require('path');
+let bcrypt = require('bcrypt');
+
 route.post('/confirm', function (req, res) {
-    user.findOneAndUpdate({ $and: [{ email: req.body.email }, { token: req.body.token }] }, { token: '-' }, function (err, doc) {
+
+    user.findOneAndUpdate({ $and: [{ email: req.body.email }, { token: req.body.token }, { password: bcrypt.hashSync(req.body.password, 10) }] }, { token: '-' }, function (err, doc) {
+
 
         if (doc) {
-            bcrypt.compare(req.body.password, doc.password, function (err, same) {
-                if (same) {
-                    let token = jwt.sign({ id: doc._id, time: Date.now() }, fs.readFileSync('./keys/Private.key'), { algorithm: "RS512" });
-                    res.cookie("token", token, {
-                        sameSite: true,
-                        httpOnly: true,
-                        maxAge: 10 * 36000
-                    });
 
-                    res.json({ 'login': true })
-                } else res.json({ 'msg': "Incorrect username and/or password" });
+            let token = jwt.sign({ id: doc._id, time: Date.now() }, fs.readFileSync('./keys/Private.key'), { algorithm: "RS512" });
+            res.cookie("token", token, {
+                sameSite: true,
+                httpOnly: true,
+                maxAge: 10 * 36000
+            });
 
-            })
-        } else res.json({ 'msg': "Invalid token" });
+            res.json({ 'login': true })
+
+
+
+        } else res.json({ 'msg': "Invalid token and/or username/password" });
     })
 })
 
