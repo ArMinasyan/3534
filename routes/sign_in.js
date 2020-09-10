9d2b63449d70dc3dfbbc75f4abb0f072a577d5d2
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-09-10 21:10:45 +0400

    change login js

--- routes/sign_in.js
+++ routes/sign_in.js
@@ -14,9 +14,9 @@ route.post('/login', [valid.log_validation], function (req, res) {
     else {
         user.findOne({ email: req.body.email }, function (err, result) {
             if (result) {
-                if (result.token == '-') {
-                    bcrypt.compare(req.body.password, result.password, function (err, same) {
-                        if (!err && same) {
+                bcrypt.compare(req.body.password, result.password, function (err, same) {
+                    if (!err && same) {
+                        if (result.token == '-') {
                             let token = jwt.sign({ id: result._id, time: Date.now() }, fs.readFileSync('./keys/Private.key'), { algorithm: "RS512" });
                             res.cookie("token", token, {
                                 sameSite: true,
@@ -24,14 +24,15 @@ route.post('/login', [valid.log_validation], function (req, res) {
                                 maxAge: 10 * 36000
                             });
                             res.json({ "login": true });
+                        } else res.json({ 'msg': "Please, confirm your email for login ", "code": false })
+                    } else res.json({ "msg": "Incorrect email and/or password" });
+                })
 
-                        } else res.json({ "msg": "Incorrect email and/or password" });
-                    })
-                } else res.json({ 'msg': "Please, confirm your email for login ", "code": false })
             } else res.json({ "msg": "Incorrect email and/or password" });
-
         })
     }
 })
+
+
 
 module.exports = route;
